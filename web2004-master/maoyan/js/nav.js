6bfd20cf2428248ba4144141fb60ebb33f2244a1
张宇轩 <67316588+eison-ty@users.noreply.github.com>
2020-07-08 20:58:26 +0800

    Add files via upload
    猫眼v2

---
+++ web2004-master/maoyan/js/nav.js
@@ -0,0 +1,17 @@
+var gettime=()=>{
+    var myDate=new Date();
+    var mymonth=myDate.getMonth()+1;
+    var myday=myDate.getDate();
+    if(mymonth<10){
+        mymonth="0"+mymonth;
+    }
+    var myday=myDate.getDate();
+    if(myday<10){
+        myday="0"+myday;
+    }
+    var mydate=myDate.getFullYear()+"-"+mymonth+"-"+myday;
+    var mytime=myDate.toLocaleTimeString('chinese',{hour12:false}); 
+    console.log(mydate,mytime);
+    document.getElementById("get_date").innerHTML=mydate;
+    document.getElementById("get_time").innerHTML=mytime;
+}
